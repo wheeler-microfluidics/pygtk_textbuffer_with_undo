a4c4f56ba69c899745b7c73adfc0a607168310a8
Christian Fobel <christian@fobel.net>
2014-02-07 08:12:31 -0500

    Restructure as Python package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(name='textbuffer_with_undo',
+      version='0.0.1',
+      description='GTK textbuffer with undo functionality.',
+      keywords='gtk undo buffer text input widget',
+      author='Florian Heinle and Christian Fobel',
+      url='http://github.com/cfobel/pygtk_textbuffer_with_undo.git',
+      license='GPL',
+      packages=['textbuffer_with_undo'])
