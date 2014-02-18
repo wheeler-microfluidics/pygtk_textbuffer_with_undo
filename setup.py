321429f157fcdd82a55f08869459c1494c376459
Christian Fobel <christian@fobel.net>
2014-02-18 11:54:59 -0500

    Add auto git-describe version for Python packag...
    Add automatic Python package versioning using `git-describe` through the
    `version.py` module from [here][1].
    
    A Python package source distribution can be built using the following
    command:
    
        python setup.py sdist
    
    __NB__ The version strings generated by `version.getVersion()` conform
    to [PEP386][2].
    
    [1]: https://gist.github.com/mina86/8782771
    [2]: http://www.python.org/dev/peps/pep-0386

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from distutils.core import setup
 
 
-setup(name='textbuffer_with_undo',
+setup(name='pygtk-textbuffer-with-undo',
       version='0.0.1',
       description='GTK textbuffer with undo functionality.',
       keywords='gtk undo buffer text input widget',
       author='Florian Heinle and Christian Fobel',
+      author_email='christian at fobel dot net',
       url='http://github.com/cfobel/pygtk_textbuffer_with_undo.git',
       license='GPL',
       packages=['textbuffer_with_undo'])
